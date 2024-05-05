526f9ab62d0ff1810c02ce407a4d96a7f8ec6bf1
jaco-L8 <alajmany1@gmail.com>
2024-05-05 17:08:44 +0100

    Add new plants router and update styles for plant display

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Plant Findr!' });
+  var plantsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/plants.json'), 'utf8'));
+  res.render('index', { title: 'Plant Findr!', plants: plantsData });
 });
 
 module.exports = router;
