5eec6ab022a5907b1ab0d01bfe2e85e0211b4ea5
jaco <102189159+jaco-L8@users.noreply.github.com>
2024-05-13 15:59:38 +0100

    some changes, please dont push over

---
+++ models/plants.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let plantSchema = new Schema({
+
+    plantName: {
+        type: String,
+        required: true
+    },
+    dateTime: {
+        type: Date,
+    },
+    plantSize: {
+        type: String,
+    },
+    plantCharacteristics: {
+        type: String,
+    },
+    plantPhoto: {
+        type: String,
+    },
+    url: {
+        type: String,
+    },
+    address: {
+        type: String,
+    }
+
+});
+
+plantSchema.set('toObject', { getters: true, virtuals: true });
+
+let Plant = mongoose.model('Plant', plantSchema);
+
+module.exports = Plant;
